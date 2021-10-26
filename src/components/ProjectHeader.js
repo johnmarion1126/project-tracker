913e650f8b9f309b5bb48e128e33dc230846607a
John Marion Sanchez <johnmarion1126@gmail.com>
2021-10-25 23:45:20 -0700

    Add example text to header

--- src/components/ProjectHeader.js
+++ src/components/ProjectHeader.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const ProjectHeader = () => (
   <div className="project-div header">
-    Project Header
+    <h1 className="header__title">Project Title</h1>
+    <p className="header__text">This is an example project to test out the features of the project planner application.</p>
   </div>
 );
 
