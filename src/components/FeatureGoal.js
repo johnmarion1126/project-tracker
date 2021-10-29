16d3c7dfe5c28d1d2cf9fa854848b538f0d7933d
John Marion Sanchez <johnmarion1126@gmail.com>
2021-10-28 23:04:34 -0700

    Allow button to add feature goals to stage

--- src/components/FeatureGoal.js
+++ src/components/FeatureGoal.js
@@ -4,9 +4,9 @@ import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import WorkItem from './WorkItem';
 
-const FeatureGoal = ({ title }) => (
+const FeatureGoal = () => (
   <div className="feature-goal">
-    <h4 className="title feature-title">{title}</h4>
+    <h4 className="title feature-title">Example Title</h4>
   </div>
 );
 
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Components
-// eslint-disable-next-line no-unused-vars
 import FeatureGoal from './FeatureGoal';
 
-const Stage = ({ name }) => (
-  <div className="stage">
-    <h4 className="title">
-      {name}
-      <button className="add-btn" type="button">+</button>
-    </h4>
-  </div>
-);
+const Stage = ({ name }) => {
+  const [isAdding, setIsAdding] = useState(false);
+
+  return (
+    <div className="stage">
+      <h4 className="title">
+        {name}
+        <button
+          className="add-btn"
+          type="button"
+          onClick={() => setIsAdding(true)}
+        >
+          +
+        </button>
+      </h4>
+      {isAdding && <FeatureGoal />}
+    </div>
+  );
+};
 
 export default Stage;
