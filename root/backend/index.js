418d641d3f5c7005b7655c10ac6482760b30fa50
John Marion Sanchez <johnmarion1126@gmail.com>
2021-11-06 20:00:04 -0700

    Add more structure to directory

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(cors());
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`running on ${PORT}`));
