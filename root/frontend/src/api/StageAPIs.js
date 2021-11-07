1e7563024206bcc9f8891647a3690563fe37379a
John Marion Sanchez <johnmarion1126@gmail.com>
2021-11-07 00:16:22 -0700

    Create file to handle api calls in frontend

---
+++ root/frontend/src/api/StageAPIs.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const createPost = (newStage) => {
+  axios.post('http://localhost:4000/add_stage', newStage);
+};
+
+export default createPost;
+
+// TODO: Connect createPost to form
+// TODO: Create get request to fetch saved items
+// TODO: Call createPost if get request returns nothing
+// TODO: Create a put request to update items
