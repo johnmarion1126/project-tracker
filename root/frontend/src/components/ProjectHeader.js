6fd336ec1cc73197f41bd1c42acebddbb50eb09b
John Marion Sanchez <johnmarion1126@gmail.com>
2021-11-22 22:52:29 -0800

    Re-wrote description

--- root/frontend/src/components/ProjectHeader.js
+++ root/frontend/src/components/ProjectHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectHeader = () => (
   <div className="project-div header">
     <h1 className="header__title">Project Tracker</h1>
-    <p className="header__text">A Kanban-style project tracker application to manage features and work items for personal projects.</p>
+    <p className="header__text">A Kanban-style, To-Do application to list and track the progress of features for personal projects.</p>
   </div>
 );
 
