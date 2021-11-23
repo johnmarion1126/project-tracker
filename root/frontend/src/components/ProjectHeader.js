ca40c15a399553ee45a1798b6017c28658c0e67c
John Marion Sanchez <johnmarion1126@gmail.com>
2021-11-22 22:44:13 -0800

    Update header title and description

--- root/frontend/src/components/ProjectHeader.js
+++ root/frontend/src/components/ProjectHeader.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ProjectHeader = () => (
   <div className="project-div header">
-    <h1 className="header__title">Project Title</h1>
-    <p className="header__text">This is an example project to test out the features of the project planner application.</p>
+    <h1 className="header__title">Project Tracker</h1>
+    <p className="header__text">A Kanban-style project tracker application to manage features and work items for personal projects.</p>
   </div>
 );
 
